875df3d5a2ea918c4c948e601719b61c95654740
David Gao <davidgao1013@gmail.com>
2023-07-02 00:21:27 +0800

    Update version

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,5 +1,5 @@
 import { defineUserConfig } from '@vuepress/cli'
-// import { defaultTheme } from '@vuepress/theme-default'
+import { defaultTheme } from '@vuepress/theme-default'
 
 import {
   // head,
@@ -9,28 +9,25 @@ import {
   sidebarZh,
 } from './configs/index.js'
 
-import pkg from '@vuepress/theme-default';
-const { defaultTheme } = pkg;
 
 export default defineUserConfig({
   lang: 'zh-CN',
   title: '七圣召唤规则集',
   description: 'Just playing around',
   base: '/',
-
-  // theme: defaultTheme({
-  //   // logo: '/images/hero.png',
-  //   repo: 'paladin1013/genius-invokation-gym',
-  //   docsDir: 'docs',
-  //   locales: {
-  //     '/': {
-  //       // // navbar
-  //       // navbar: navbarEn,
-  //       // sidebar
-  //       sidebar: sidebarZh,
-  //       // page meta
-  //       editLinkText: '在Github中编辑',
-  //     },
-  //   },
-  // })
+  theme: defaultTheme({
+    // logo: '/images/hero.png',
+    repo: 'paladin1013/genius-invokation-rule-set',
+    docsDir: 'docs',
+    locales: {
+      '/': {
+        // // navbar
+        // navbar: navbarEn,
+        // sidebar
+        sidebar: sidebarZh,
+        // page meta
+        editLinkText: '在Github中编辑',
+      },
+    },
+  })
 })
